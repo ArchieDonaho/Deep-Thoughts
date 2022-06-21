5e12b55f71cbc90860a84febe6baa644c035df2f
AD <archied5150@yahoo.com>
2022-06-21 14:40:20 -0500

    added single page thoughts and user pages

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,15 @@ import {
   InMemoryCache, // enables Apollo client instance to cache API response data
   createHttpLink, // allows us to control how the Apollo client makes a request, like middleware
 } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 // create link to the api
 const httpLink = createHttpLink({
@@ -24,13 +30,26 @@ function App() {
   return (
     // we contain everything in the ApolloProvider tag so all elements have access to the client tag
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      {/* everything wrapped in the Router is aware of the client side routing that can take place */}
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/profile'>
+                <Route path=':username' element={<Profile />} />
+                <Route path='' element={<Profile />} />
+              </Route>
+              <Route path='/thought/:id' element={<SingleThought />} />
+              <Route path='*' element={<NoMatch />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import ReactionList from '../components/ReactionList';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client'; // allow us to use our query to retrieve info
+import { QUERY_THOUGHT } from '../utils/queries'; // import our query
 
-const SingleThought = props => {
+const SingleThought = (props) => {
+  // destructure the parameter info from the url
+  const { id: thoughtId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    variables: { id: thoughtId }, // the "id" property becomes the "$id" property on the GraphQL query
+  });
+
+  const thought = data?.thought || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <div className="card mb-3">
-        <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
-            Username
+      <div className='card mb-3'>
+        <p className='card-header'>
+          <span style={{ fontWeight: 700 }} className='text-light'>
+            {thought.username}
           </span>{' '}
-          thought on createdAt
+          thought on {thought.createdAt}
         </p>
-        <div className="card-body">
-          <p>Thought Text</p>
+        <div className='card-body'>
+          <p>{thought.thoughtText}</p>
         </div>
       </div>
+      {thought.reactionCount > 0 && (
+        <ReactionList reactions={thought.reactions} />
+      )}
     </div>
   );
 };
